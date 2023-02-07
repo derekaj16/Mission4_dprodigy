5e55352ac18d6c254ae62dfd1645f081b80d4dc2
derekaj16 <dprodigy@byu.edu>
2023-02-06 22:56:36 -0700

    change to MVC

---
+++ Mission4_dprodigy/Controllers/BlahController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_dprodigy.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+    }
+
+}
